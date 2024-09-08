4ad6027cb39fb4c504d2428dd126769b176b41a5
Orhant <orhantornaci@gmail.com>
2024-09-08 12:39:48 +0300

    .

--- 03_webserver.sh
+++ 03_webserver.sh
@@ -2,6 +2,7 @@
 
 # Nginx Kurulumu
 echo "Nginx kuruluyor..."
+sudo apt update
 sudo apt install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
@@ -11,7 +12,7 @@ echo "UFW Nginx profilleri ile yapılandırma yapılıyor..."
 sudo ufw app list
 
 # Eğer 'Nginx Full' profili yoksa HTTP ve HTTPS portlarını aç
-if ! sudo ufw app info 'Nginx Full'; then
+if ! sudo ufw app info 'Nginx Full' &>/dev/null; then
     sudo ufw allow 'Nginx HTTP'
     sudo ufw allow 'Nginx HTTPS'
 else
@@ -36,5 +37,19 @@ sudo mysql_secure_installation
 # MariaDB'ye Kök Kullanıcısı Olarak Giriş Yapma
 echo "MariaDB'ye kök kullanıcısı olarak giriş yapılacak..."
 sudo mysql -u root -p -e "SELECT USER();"
+
+# Root kullanıcısı için yeni şifre belirleme
+read -sp "MariaDB root kullanıcısı için yeni bir şifre girin: " NEW_ROOT_PASSWORD
+echo
+read -sp "Şifreyi tekrar girin: " NEW_ROOT_PASSWORD_CONFIRM
+echo
+
+if [ "$NEW_ROOT_PASSWORD" != "$NEW_ROOT_PASSWORD_CONFIRM" ]; then
+    echo "Şifreler uyuşmuyor. Lütfen tekrar deneyin."
+    exit 1
+fi
+
+echo "MariaDB root kullanıcısı şifresi ayarlanıyor..."
+sudo mysql -u root -p -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$NEW_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
 
 echo "Kurulum ve yapılandırma tamamlandı."
